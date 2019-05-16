306d4f4ed2e7ede2c04532c64bb208eeef28d7c4
Matheus <Binotto>
2019-05-16 19:04:39 -0300

    Setting up Babel

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,13 @@
+'use strict';
+
 console.log('App.js is running!');
 
 //JSX - Javascript XML
-//var template= <p>This is JSX from app.js!</p>;
-var template = React.createElement("h1", {
-    id: "someid"
-  }, "Something new");
-
-var appRoot= document.getElementById('app');
+var template = React.createElement(
+  'h1',
+  null,
+  'Indecision App'
+);
+var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
