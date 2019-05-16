e4c68c70f33a12415c9bd07e327e9c0a8d20923e
Matheus <Binotto>
2019-05-16 20:01:43 -0300

    Exploring JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,10 +4,55 @@ console.log('App.js is running!');
 
 //JSX - Javascript XML
 var template = React.createElement(
-  'h1',
-  null,
-  'Indecision App'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some info'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
 );
+
+//Templatetwo var JSX expression
+var templatetwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Matheus Binotto'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: 25'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: Curitiba'
+    )
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templatetwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,27 @@
 console.log('App.js is running!');
 
 //JSX - Javascript XML
-var template = <h1>Indecision App</h1>;
+var template =( 
+<div>
+    <h1>Indecision App</h1>
+    <p>This is some info</p>
+    <ol>
+        <li>Item one</li>
+        <li>Item two</li>
+    </ol>
+</div>);
+
+
+//Templatetwo var JSX expression
+var templatetwo =( 
+    <div>
+        <h1>Matheus Binotto</h1>
+        <p>Age: 25</p>
+        <p>Location: Curitiba</p>
+    </div>
+);
+
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templatetwo, appRoot);
