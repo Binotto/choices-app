c9b82ccf35f1f653540ba62595372717df80c4f7
Matheus <Binotto>
2019-05-18 23:44:19 -0300

    Forms and Inputs

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,91 +4,86 @@ console.log('App.js is running!');
 
 var app = {
     title: 'Indecision app',
-    subtitle: 'informations',
-    options: ['One, Two']
+    subtitle: 'Put you life in the hands of a computer.',
+    options: []
 };
 
-//JSX - Javascript XML
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        app.title
-    ),
-    app.subtitle && React.createElement(
-        'p',
-        null,
-        app.subtitle
-    ),
-    React.createElement(
-        'p',
-        null,
-        app.options.length > 0 ? 'Here are you options:' : 'No Options'
-    ),
-    React.createElement(
-        'ol',
-        null,
-        React.createElement(
-            'li',
-            null,
-            'Item one'
-        ),
-        React.createElement(
-            'li',
-            null,
-            'Item two'
-        )
-    )
-);
+var onFormSubmit = function onFormSubmit(e) {
+    //Prevents full page refresh
+    e.preventDefault();
 
-var count = 0;
-var addOne = function addOne() {
-    console.log('addOne');
-    count++;
-    renderCounterApp();
-};
-var minusOne = function minusOne() {
-    console.log('minusOne');
-    count--;
-    renderCounterApp();
-};
-var reset = function reset() {
-    console.log('reset');
-    count = 0;
-    renderCounterApp();
+    var option = e.target.elements.option.value;
+
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        render();
+    }
 };
 
-var appRoot = document.getElementById('app');
+var onRemoveAll = function onRemoveAll() {
+    app.options = [];
+    render();
+};
 
-var renderCounterApp = function renderCounterApp() {
-    var templateTwo = React.createElement(
+var render = function render() {
+    //JSX - Javascript XML
+    var template = React.createElement(
         'div',
         null,
         React.createElement(
             'h1',
             null,
-            'Count: ',
-            count
+            app.title
+        ),
+        app.subtitle && React.createElement(
+            'p',
+            null,
+            app.subtitle
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options.length > 0 ? 'Here are you options:' : 'No Options'
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options.length
         ),
         React.createElement(
             'button',
-            { onClick: addOne },
-            '+1'
+            { onClick: onRemoveAll },
+            'Remove All Options'
         ),
         React.createElement(
-            'button',
-            { onClick: minusOne },
-            '-1'
+            'ol',
+            null,
+            React.createElement(
+                'li',
+                null,
+                'Item one'
+            ),
+            React.createElement(
+                'li',
+                null,
+                'Item two'
+            )
         ),
         React.createElement(
-            'button',
-            { onClick: reset },
-            'Reset'
+            'form',
+            { onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'option' }),
+            React.createElement(
+                'button',
+                null,
+                'Add option'
+            )
         )
     );
-    ReactDOM.render(templateTwo, appRoot);
+
+    ReactDOM.render(template, appRoot);
 };
 
-renderCounterApp();
+var appRoot = document.getElementById('app');
+render();
--- src/app.js
+++ src/app.js
@@ -2,21 +2,54 @@ console.log('App.js is running!');
 
 const app = {
     title: 'Indecision app',
-    subtitle: 'informations',
-    options: ['One, Two']
+    subtitle: 'Put you life in the hands of a computer.',
+    options: []
 };
 
-//JSX - Javascript XML
-const template =( 
-<div>
-    <h1>{app.title}</h1>
-    {app.subtitle && <p>{app.subtitle}</p>}
-    <p>{app.options.length > 0 ? 'Here are you options:' : 'No Options'}</p>
-    <ol>
-        <li>Item one</li>
-        <li>Item two</li>
-    </ol>
-</div>);
+const onFormSubmit = (e) => {
+    //Prevents full page refresh
+    e.preventDefault();
+
+    const option = e.target.elements.option.value;
+
+    if(option){
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        render();
+    }
+}
+
+const onRemoveAll = () => {
+    app.options = [];
+    render()
+}
+
+const render = () => {
+    //JSX - Javascript XML
+    const template =( 
+        <div>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            <p>{app.options.length > 0 ? 'Here are you options:' : 'No Options'}</p>
+            <p>{app.options.length}</p>
+            <button onClick={onRemoveAll}>Remove All Options</button>
+            <ol>
+                <li>Item one</li>
+                <li>Item two</li>
+            </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option"/>
+                <button>Add option</button>
+            </form>
+        </div>);
+        
+        ReactDOM.render(template, appRoot);
+}
 
 const appRoot = document.getElementById('app');
+render();
+
+
+
+
 
