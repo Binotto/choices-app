19048d301d47cb779de179e5b17ca415c81db44c
Matheus <Binotto>
2019-05-23 16:40:05 -0300

    Installing & Configuring Webpack III

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,11 @@
+const path = require('path')
 // entry -> output
+
+module.exports = {
+    mode: 'production',
+    entry: './src/app.js',
+    output:{
+        path: path.join(__dirname, 'public/'),
+        filename: 'bundle.js'
+    }
+}
