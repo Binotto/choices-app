544987207e7726c0f75788d4e141f69bf45a58f9
Matheus <Binotto>
2019-05-23 18:19:13 -0300

    ES6 import/export II

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
-//import './utils.js'
-import { square, add } from './utils.js';
-console.log('app.js is running ');
-console.log(square(7));
-console.log(add(7, 7))
+// import './utils.js'
+// import { square, add } from './utils.js';
+// console.log('app.js is running ');
+// console.log(square(7));
+// console.log(add(7, 7))
+
+
+import { isAdult, canDrink } from './person.js';
+
+console.log(isAdult(25));
+console.log(canDrink(25));
+
+
---
+++ src/person.js
@@ -0,0 +1,7 @@
+// export const isAdult = (age) => age >= 18;
+// export const canDrink = (age) => age >= 18;
+//////////////////////////////////////////////
+const canDrink = (age) => age >= 18;
+const isAdult = (age) => age >= 21;
+
+export{ canDrink, isAdult};
