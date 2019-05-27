73090f9a1084a29a605abc781c8852804e953883
Matheus <Binotto>
2019-05-27 15:53:22 -0300

    Testing - REACT/REACTDOM import

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
 //install -> import -> use
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+const template = React.createElement('p', {}, 'test');
+ReactDOM.render(template, document.getElementById('app'));
