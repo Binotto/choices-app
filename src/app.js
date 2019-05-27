714b78b9588d24c7b4af30e20121d9558e845bc9
Matheus <Binotto>
2019-05-27 15:23:45 -0300

    Importing the Validator

--- src/app.js
+++ src/app.js
@@ -1,15 +1,2 @@
-// import './utils.js'
-// import anythingIWant,{ square, add } from './utils.js';
-// console.log('app.js is running ');
-// console.log(square(7));
-// console.log(add(7, 7));
-// console.log(anythingIWant(700,7));
-
-
-import isSenior, { isAdult, canDrink } from './person.js';
-
-console.log(isAdult(25));
-console.log(canDrink(25));
-console.log(isSenior(65))
-
-
+//install -> import -> use
+import validator from 'validator';
