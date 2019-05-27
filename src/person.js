9e9ef2654064969b8f7b33e9615ca4e36fb01891
Matheus <Binotto>
2019-05-27 17:20:52 -0300

    Deleting Unnecessary Code

--- src/person.js
+++
@@ -1,8 +0,0 @@
-// export const isAdult = (age) => age >= 18;
-// export const canDrink = (age) => age >= 18;
-//////////////////////////////////////////////
-const canDrink = (age) => age >= 18;
-const isAdult = (age) => age >= 21;
-const isSeniorCitizen = (age) => age >= 65;
-
-export{ canDrink, isAdult, isSeniorCitizen as default};
