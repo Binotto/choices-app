779e7ad173e7f7622826cc1ac51c96b376231d39
Matheus <Binotto>
2019-05-27 17:44:07 -0300

    Creating the Option Component File

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AddOption from './components/AddOption';
+import Option from './components/Option';
 
 class IndecisionApp extends React.Component{
     constructor(props){
@@ -127,20 +128,7 @@ const Options = (props) => {
 }
 
 
-const Option = (props) => {
-    return (
-        <div>
-            {props.optionText}
-            <button 
-                onClick={(e) => {
-                    props.handleDeleteOption(props.optionText);
-                }}
-            >
-                Remove
-            </button>
-        </div>
-    );
-}
+
 
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
