999c10927878690174c4d4530fa4492ffa83b7e3
Matheus <Binotto>
2019-05-27 22:30:46 -0300

    ES6 class properties - Example I

--- src/app.js
+++ src/app.js
@@ -4,3 +4,22 @@ import IndecisionApp from './components/IndecisionApp';
 
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
+
+
+class OldSyntax {
+    constructor(){
+        this.name = 'Matheus Binotto';
+    }
+}
+const oldSyntax = new OldSyntax();
+console.log(oldSyntax);
+
+
+// ----------
+
+class NewSyntax {
+    name = 'Yzadora';
+}
+
+const newSyntax = new NewSyntax();
+console.log(newSyntax);
