7802a0cc1856357dbc14515b6494d46a38aa1805
Matheus <Binotto>
2019-05-29 14:02:01 -0300

    Passing Children to Component - Saving the example

--- src/app.js
+++ src/app.js
@@ -2,22 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
-
-const Layout = (props) => {
-    return(
-        <div>
-            <p>Header</p>
-            {props.children}
-            <p>Footer</p>
-        </div>
-    );
-}
-
-ReactDOM.render((
-    <Layout>
-    <div>
-        <div>Title</div>
-        <p>This is my page</p>
-    </div>
-    </Layout>
-), document.getElementById('app'))
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
