93b4a8dc1ae1dcf155d60e2e6c7e69f6e4a71161
Matheus <Binotto>
2019-05-30 20:34:40 -0300

    Styling Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,12 @@ import React from 'react';
 const Footer = (props) => (
     <div className="footer">
         <div className="container">
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
             <h1 className="footer__title">{props.title}</h1>
-            {props.subtitle_footer && <h2 className="footer__subtitle">{props.subtitle_footer}</h2>}
+            {props.subtitle_footer && <a href="https://github.com/Binotto/indecision-app" className="footer__subtitle footer_github">
+                <i className="fa fa-github"></i>
+                {props.subtitle_footer}
+            </a>}
         </div>
     </div>
 );
