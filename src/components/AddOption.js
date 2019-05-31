c00b1575c5babfce7ec7ef946113fdd34745764f
Matheus <Binotto>
2019-05-31 12:00:27 -0300

    Limiting the input value of AddOption

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -25,7 +25,7 @@ export default class AddOption extends React.Component{
             <div>
                 {this.state.error && <p className="add-option-error">{this.state.error}</p>}
                 <form className="add-option"onSubmit={this.handleAddOption}>
-                    <input className="add-option__input" type="text" name="option"></input>
+                    <input className="add-option__input" type="text" name="option" maxlength="100"></input>
                     <button className="button">Add Option</button>
                 </form>
             </div>
