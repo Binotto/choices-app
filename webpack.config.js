8d586304f98cea7648e0268afd8e80a5eb844064
Matheus <Binotto>
2019-05-31 17:39:25 -0300

    Production Webpack: webpack.config.js converting module.exports to arrow function

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,35 @@
 const path = require('path')
 // entry -> output
 
-module.exports = {
-    mode: 'production',
-    entry: './src/app.js',
-    output:{
-        path: path.join(__dirname, 'public/'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test:/\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase:path.join(__dirname, 'public/')
-    }
-}
+module.exports = () => {
+    return {
+        mode: 'production',
+        entry: './src/app.js',
+        output:{
+            path: path.join(__dirname, 'public/'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test:/\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }]
+        },
+        devtool: 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase:path.join(__dirname, 'public/')
+        }
+    };
+};
+
+
 
 //loader
