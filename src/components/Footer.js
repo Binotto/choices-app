fa8eb43a540b87358ef06ccef27d6d846ad4a35f
Matheus <Binotto>
2019-05-31 12:03:55 -0300

    Changing the link of the icons to an index.html

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Footer = (props) => (
     <div className="footer">
         <div className="container">
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
             <h1 className="footer__title">{props.title}</h1>
             {props.subtitle_footer && <a href="https://github.com/Binotto/indecision-app" className="footer__subtitle footer_github">
                 <i className="fa fa-github"></i>
